ffc15744a22282006051e9bf53d50c9c82d96a5b
Jamie Love <Jamie.Love@charter.com>
2020-01-11 13:20:47 -0700

    add delete query and mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -3,13 +3,44 @@ const Mutations = {
   // use async / await because the thing returned to createItem is a Promise
   async createItem(parent, args, ctx, info) {
     // TODO: check if they are logged in
-    const item = await ctx.db.mutation.createItem({
-      data: {
-        ...args
-      }
-    }, info);
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
     return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates since we're not updating the ID
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    // set this up assuming we have authentication
+    const where = { id: args.id };
+    // 1. Find the item
+    const item = await ctx.db.query.item({ where }, `{id, title}`);
+    // 2. Check if they have permissions to delete it
+    // TODO
+    // 3. Delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
